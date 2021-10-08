787f73833d2bfeb7aa3ded696f90ef13ba063c42
许鸿志 <847118663@qq.com>
2021-10-08 15:06:52 +0800

    生成的也添加进来

---
+++ business/src/main/java/cn/piao888/business/BusinessApplication.java
@@ -0,0 +1,14 @@
+package cn.piao888.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Role;
+
+@SpringBootApplication
+public class BusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+
+}
